ff3d7f78466d06ea902dd1fa9d20e88ed281d8c2
misodengaku <misodengaku@gmail.com>
2014-05-05 03:20:06 +0900

    田端からパクった(作業中)

--- hanayo.rb
+++ hanayo.rb
@@ -1,59 +1,66 @@
 # -*- coding: utf-8 -*-
-require 'rubygems'
-require "RMagick"
 require "open-uri"
-require "tweetstream"
-require "twitter"
+require 'yaml'
+require "bundler"
+Bundler.require
 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-OAUTH_TOKEN = ""
-OAUTH_SECRET = ""
 
-Twitter.configure do |config|
-  config.consumer_key = CONSUMER_KEY
-  config.consumer_secret = CONSUMER_SECRET
-  config.oauth_token = OAUTH_TOKEN
-  config.oauth_token_secret = OAUTH_SECRET
+#設定ファイルロード
+begin
+	settings = YAML::load(open("./hanayo.conf"))
+rescue
+	puts "[#{Time.now}]: [ERROR] config file load failed."
 end
- 
+puts "[#{Time.now}]: config file loaded."
+# Twiter関連モジュール準備
 TweetStream.configure do |config|
-  config.consumer_key = CONSUMER_KEY
-  config.consumer_secret = CONSUMER_SECRET
-  config.oauth_token = OAUTH_TOKEN
-  config.oauth_token_secret = OAUTH_SECRET
-  config.auth_method  = :oauth
+	config.consumer_key			= settings["consumer_key"]
+	config.consumer_secret		= settings["consumer_secret"]
+	config.oauth_token			= settings["oauth_token"]
+	config.oauth_token_secret	= settings["oauth_token_secret"]
+	config.auth_method			= :oauth
+end
+
+twitter = Twitter::REST::Client.new do |config|
+	config.consumer_key			= settings["consumer_key"]
+	config.consumer_secret		= settings["consumer_secret"]
+	config.access_token			= settings["oauth_token"]
+	config.access_token_secret	= settings["oauth_token_secret"]
 end
 
 def dot(status)
-  url=status.profile_image_url.gsub("_normal","")
-  filename=File.basename(url)
-  
-  # リプライしてきたユーザーのアイコン画像を保存
-  open(filename, 'wb') do |output|
-    open(url) do |data|
-      output.write(data.read)
-    end
-  end
-  
-  # なんかいい感じにする
-  hana = Magick::ImageList.new("hanayo.png")
-  image = Magick::ImageList.new(filename)
-  image = image.resize(397.7 / image.rows).rotate(7.604)
-  image = hana.composite(image, 350, 182, Magick::OverCompositeOp).composite(hana, 0, 0, Magick::OverCompositeOp)
-  image.write("tmp.jpg")
-  
-  # 送りつける どうぞ！
-  Twitter.update_with_media("@#{status.user.screen_name} どうぞ！", File.open("tmp.jpg"), :in_reply_to_status_id => status.id)
+	url = status.user.profile_image_url.to_s.gsub("_normal","")
+	filename = File.basename(url)
+	# リプライしてきたユーザーのアイコン画像を保存
+	open(filename, 'wb') do |output|
+		open(url) do |data|
+			output.write(data.read)
+		end
+	end
+	
+	# なんかいい感じにする
+	puts "b"
+	image = Magick::ImageList.new(filename)
+	puts "a"
+	hana = Magick::ImageList.new("hanayo.png")
+	p image
+	image = image.resize(397.7 / image.rows).rotate(7.604)
+	image = hana.composite(image, 350, 182, Magick::OverCompositeOp).composite(hana, 0, 0, Magick::OverCompositeOp)
+	image.write("tmp.jpg")
+	p image
+	# 送りつける どうぞ！
+	twitter.update_with_media("@#{status.user.screen_name} どうぞ！", File.open("tmp.jpg"), :in_reply_to_status_id => status.id)
 end
 
-my_sn = Twitter.user.screen_name
+my_sn = twitter.user.screen_name
 
 client = TweetStream::Client.new
 client.userstream do |status|
-  if /^@#{my_sn}\s+クソコラ.*$/i =~ status.text
-    print "dot - "
-    puts status.user.screen_name
-    dot(status)
-  end
+	#デバッグ中
+	#if /^@#{my_sn}\s+クソコラ.*$/i =~ status.text
+	if /^@#{my_sn}\s+a.*$/i =~ status.text
+		print "dot - "
+		puts status.user.screen_name
+		dot(status)
+	end
 end
